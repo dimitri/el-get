75b1fa3ca696a9139fa5c196056c466a92e00dd9
Dimitri Fontaine <dim@DimIMac.local>
2010-06-27 21:13:22 +0200

    Second run on el-get, allow it to:  - fetch single HTTP file.el  - require a give set of features  - run a list of build commands for the package, at install  - call hooks when install is done  - setup load-path and Info-directory-list

--- el-get.el
+++ el-get.el
@@ -6,7 +6,7 @@
 ;;
 ;; Author: Dimitri Fontaine <dim@tapoueh.org>
 ;; URL: http://www.emacswiki.org/emacs/el-get.el
-;; Version: 0.1
+;; Version: 0.2
 ;; Created: 2010-06-17
 ;; Keywords: emacs elisp install elpa git apt-get debian
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
@@ -14,17 +14,36 @@
 ;; This file is NOT part of GNU Emacs.
 ;;
 ;; Install:
-;;  (require 'dim-installer)
+;;  (require 'el-get)
+;;  then define your el-get-sources
 
 (require 'dired-x) ; dired-make-relative-symlink
 
 (defgroup el-get nil "el-get customization group"
   :group 'convenience)
 
+(defvar el-get-git-clone-hook       nil "Hook run after git clone.")
+(defvar el-get-apt-get-install-hook nil "Hook run after apt-get install.")
+(defvar el-get-elpa-install-hook    nil "Hook run after ELPA package install.")
+(defvar el-get-http-install-hook    nil "Hook run after http retrieve.")
+
 (defcustom el-get-methods
-  '(:git     (el-get-git-clone el-get-git-pull el-get-rmdir)
-    :apt-get (el-get-apt-get-install el-get-apt-get-install el-get-apt-get-remove)
-    :elpa    (el-get-elpa-install el-get-elpa-install el-get-rmdir))
+  '(:git     (:install el-get-git-clone 
+		       :install-hook el-get-git-clone-hook
+		       :update el-get-git-pull 
+		       :remove el-get-rmdir)
+    :apt-get (:install el-get-apt-get-install 
+		       :install-hook el-get-apt-get-install-hook
+		       :update el-get-apt-get-install
+		       :remove el-get-apt-get-remove)
+    :elpa    (:install el-get-elpa-install 
+		       :install-hook el-get-elpa-install-hook
+		       :update el-get-elpa-install
+		       :remove el-get-rmdir)
+    :http    (:install el-get-http-install
+		       :install-hook el-get-http-install-hook
+		       :update el-get-http-install
+		       :remove el-get-rmdir))
   "Register methods that el-get can use to fetch and update a given package
 
 That should be a plist of method names whose properties is a list
@@ -43,17 +62,42 @@ the second to update the package and the third to remove it."
 (defvar el-get-sources nil
   "List of sources for packages.
 
-Each package should be a cons '(name . (method . source)) where the
-source syntax depends on the method.")
+An example of each package entry is following.
+
+	(:name 'bbdb
+	       :type git
+	       :url \"git://github.com/barak/BBDB.git\"
+	       :load-path '(\"./lisp\" \"./bits\")
+	       :info \"texinfo\"
+	       :build '(\"./configure\" \"make\"))")
+
+(defun el-get-method (method-name action)
+  "return the function to call for doing action (e.g. install) in given method"
+  (let* ((method  (intern-soft (concat ":" (format "%s" method-name))))
+	 (actions (plist-get el-get-methods method)))
+    (plist-get actions action)))
 
 (defun el-get-check-init ()
   "Check that we can run el-get"
   (unless (file-directory-p el-get-dir)
     (make-directory el-get-dir)))
 
+(defun el-get-package-directory (package)
+  "Returns the package installation directory absolute name"
+  (concat (file-name-as-directory el-get-dir) package))
+
+(defun el-get-add-path-to-list (package list path)
+  "(add-to-list list path) checking for path existence within
+given package directory."
+  (let ((pdir     (el-get-package-directory package))
+	(fullpath (concat (file-name-as-directory pdir) path)))
+    (unless (file-directory-p fullpath)
+      (error "el-get could not find directory `%s' for package %s" path package))
+    (add-to-list list fullpath)))
+
 (defun el-get-package-exists-p (package)
   "true only when the given package name is either a directory or a symlink in el-get-dir"
-  (let ((pdir (concat (file-name-as-directory el-get-dir) package)))
+  (let ((pdir (el-get-package-dir package)))
     ;; seems overkill as file-directory-p will always be true
     (or (file-directory-p pdir)
 	(file-symlink-p   pdir))))
@@ -77,18 +121,19 @@ source syntax depends on the method.")
 
   (el-get-check-init)
 
-  (message "el get %S" package)
+  (message "el-get %S" package)
 
   (let ((ret (shell-command-to-string 
 	      (concat "cd " el-get-dir
 		      " && " magit-git-executable " --no-pager clone " url))))
 
     (el-get-git-symlink-package package url)
+    (run-hooks 'el-get-git-clone-hook)
     ret))
 
 (defun el-get-git-pull (package url)
   "git pull the package"
-  (let ((pdir (concat (file-name-as-directory el-get-dir) package)))
+  (let ((pdir (el-get-package-directory package)))
     (when (file-directory-p pdir)
       (shell-command-to-string 
        (concat "cd " pdir
@@ -97,7 +142,9 @@ source syntax depends on the method.")
 (defun el-get-apt-get-install (package &optional url)
   "apt-get install package, url is there for API compliance"
   (shell-command-to-string 
-   (concat "sudo " el-get-apt-get " install " package)))
+   (concat "sudo " el-get-apt-get " install " package))
+  (run-hooks 'el-get-apt-get-install-hook)
+  nil)
 
 (defun el-get-apt-get-remove (package &optional url)
   "apt-get remove package, url is there for API compliance"
@@ -108,26 +155,83 @@ source syntax depends on the method.")
   "ask elpa to install given package"
   (error "Not Implemented. Yet?"))
 
+(defun el-get-http-retrieve (package url)
+  "Get an elisp file using `url-retrieve-synchronously'. 
+
+Returns the feature provided if any"
+  (let* ((pdir   (el-get-package-directory package))
+	 (dest   (concat (file-name-as-directory pdir) package ".el")))
+    (with-current-buffer (url-retrieve-synchronously url)
+      ;; prune HTTP headers before save
+      (beginning-of-buffer)
+      (re-search-forward "^$" nil 'move)
+      (forward-char)
+      (delete-region (point-min) (point))
+      (write-file dest))))
+
+(defun el-get-http-install (package url)
+  "Dowload a single-file package over HTTP "
+  (let ((pdir   (el-get-package-directory package)))
+    (make-directory pdir)
+    (el-get-http-retrieve package url)))
+
 (defun el-get-rmdir (package url)
   "Just rm -fr the package directory"
   (error "Not Yet Implemented."))
 
+(defun el-get-build (package commands)
+  "run each command from the package directory"
+  (let ((pdir   (el-get-package-directory package)))
+    (dolist (c commands)
+      (message "el-get %s: cd %s && %s" package pdir c)
+      (message "%S" (shell-command-to-string 
+		     (concat "cd " pdir " && " c))))))
+
 (defun el-get ()
-  "Check that all sources have been initialized and init them as needed"  
+  "Check that all sources have been downloaded once, and init them as needed.
+
+This will not update the sources by using `apt-get install' or
+`git pull', but it will ensure the sources have been installed
+and will set the load-path and Info-directory-list depending on
+the el-get-sources setup."
+
   (mapcar 
    (lambda (source)
-     (let* ((package (symbol-name (car source)))
-	    (method  (caadr source))
-	    (url     (cdadr source))
-	    (pdir   
-	     (concat (file-name-as-directory el-get-dir) package)))
-       (message "%s: %s %s %s" package method url (plist-get el-get-methods method))
-       ;; if package already exists, pull it, method 1
-       ;; if it does not already exists, init it, method 0
-       (funcall 
-	(nth (if (el-get-package-exists-p package) 1 0)
-	     (plist-get el-get-methods method))
-	package url)))
+     (let* ((package  (format "%s" (plist-get source :name)))
+	    (method   (plist-get source :type))
+	    (install  (el-get-method method :install))
+	    (hooks    (el-get-method method :install-hook))
+	    (url      (plist-get source :url))
+	    (feats    (plist-get source :features))
+	    (commands (plist-get source :build))
+	    (el-path  (or (plist-get source :load-path) '(".")))
+	    (infodir  (plist-get source :info))
+	    (pdir     (el-get-package-directory package)))
+       (message "%s: %s %s %s" package method url install)
+
+       (unless (el-get-package-exists-p package)
+	 (message "Initializing package %s with function %s" package install)
+	 (funcall install package url)
+	 (run-hooks hooks)
+
+	 ;; consider building when sources are thus setup
+	 (el-get-build package commands))
+
+       ;; append entries to load-path and Info-directory-list
+       (mapc (lambda (path) 
+	       (el-get-add-path-to-list package 'load-path path))
+	     (if (stringp el-path) (list el-path) el-path))
+       (mapc (lambda (path) 
+	       (el-get-add-path-to-list package 'Info-directory-list path))
+	     (if (stringp infodir) (list infodir) infodir))
+
+       ;; if a feature is provided, require it now
+       (when feats 
+	 (mapc (lambda (feature) (message "require '%s" (require feature)))
+	       (if (symbolp feats) (list feats) feats)))
+       
+       ;; return the list of checked packages
+       package))
    el-get-sources))
 
 (provide 'el-get)
