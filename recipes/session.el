9705e52974602f2424adf0ad16b71b17c9e2a98d
Marc Weber <marco-oweber@gmx.de>
2010-09-03 04:07:36 +0200

    adding recipes for pov-mode session vimpulse

---
+++ recipes/session.el
@@ -0,0 +1,7 @@
+(:name session
+       :type http-tar
+       :options ("xzf")
+       :load-path ("lisp")
+       :url "http://downloads.sourceforge.net/project/emacs-session/session/2.2a/session-2.2a.tar.gz"
+       :info "session management"
+       )
---
+++ recipes/vimpulse.el
@@ -0,0 +1,6 @@
+(:name vimpulse
+       :type git
+       :url "git://gitorious.org/vimpulse/vimpulse.git"
+       :info "vimpulse for emacs"
+       :features vimpulse
+       )
