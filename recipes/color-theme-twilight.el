24328c493453f7ca771684adb1303911fb10cf07
Matt Swasey <matt.swasey@gmail.com>
2010-09-10 03:18:42 +0800

    added recipe for twilight color theme

---
+++ recipes/color-theme-twilight.el
@@ -0,0 +1,5 @@
+(:name color-theme-twilight
+       :type git
+       :url "http://github.com/crafterm/twilight-emacs.git"
+       :load "color-theme-twilight.el"
+       :after (lambda () (color-theme-twilight)))
