560fefd3e17661df700832033c32dd181fe94903
Dimitri Fontaine <dim@tapoueh.org>
2010-09-19 20:29:16 +0200

    Fix emacs-w3m recipe, typo added with build/darwin support.

--- recipes/emacs-w3m.el
+++ recipes/emacs-w3m.el
@@ -4,6 +4,6 @@
       :options "login"
       :url ":pserver:anonymous@cvs.namazu.org:/storage/cvsroot"
       :build ("autoconf" "./configure" "make")
-      :build/darwin ("autoconf" "./configure --with-emacs=/Applications/Emacs.app/Contents/MacOS/Emacs" "make"))
+      :build/darwin ("autoconf" "./configure --with-emacs=/Applications/Emacs.app/Contents/MacOS/Emacs" "make")
       :info "doc"
       :features "w3m-load")
