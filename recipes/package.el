7736b0875a77dbf7c9ceeba20f001b9faaff0095
Dimitri Fontaine <dim@tapoueh.org>
2010-09-20 10:00:52 +0200

    Complete the package (ELPA) recipe to a full install.

--- recipes/package.el
+++ recipes/package.el
@@ -1,3 +1,7 @@
 (:name package
        :type http
-       :url "http://tromey.com/elpa/package.el")
+       :url "http://tromey.com/elpa/package.el"
+       :features package
+       :after (lambda ()
+                (make-directory (expand-file-name "~/.emacs.d/elpa") t)
+                (package-initialize)))
