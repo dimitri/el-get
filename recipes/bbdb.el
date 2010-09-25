ca4b25640fdc0c9458551e410eb70d2196cdfee6
Jim Crossley <jim@crossleys.org>
2010-09-25 18:32:40 -0400

    Fix the bbdb build on linux, and initialize after installing.

--- recipes/bbdb.el
+++ recipes/bbdb.el
@@ -2,7 +2,9 @@
        :type git
        :url "git://github.com/barak/BBDB.git"
        :load-path ("./lisp" "./bits")
-       :build ("./configure" "make")
+       :build ("./configure" "make autoloads" "make")
        :build/darwin ("./configure --with-emacs=/Applications/Emacs.app/Contents/MacOS/Emacs" "make autoloads" "make")
+       :features bbdb
+       :after (lambda () (bbdb-initialize))
        :info "texinfo")
 
