4a551ab28b89d27326d5084750c63dc7792d3528
Jim Crossley <jim@crossleys.org>
2010-09-25 18:34:17 -0400

    Anonymous login should be sufficient, so don't prompt user.
    Prompting is a little strange in async mode, I think.

--- recipes/emacs-w3m.el
+++ recipes/emacs-w3m.el
@@ -1,7 +1,6 @@
 (name :emacs-w3m
       :type cvs
       :module "emacs-w3m"
-      :options "login"
       :url ":pserver:anonymous@cvs.namazu.org:/storage/cvsroot"
       :build ("autoconf" "./configure" "make")
       :build/darwin ("autoconf" "./configure --with-emacs=/Applications/Emacs.app/Contents/MacOS/Emacs" "make")
