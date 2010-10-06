b0471c53b8812cb67ae0ecc2ac3f2ad19696e580
Marc Weber <marco-oweber@gmx.de>
2010-10-07 03:28:01 +0800

    ensime: enable scala-mode and document where to find it

--- recipes/ensime.el
+++ recipes/ensime.el
@@ -6,4 +6,6 @@
  :load-path ("./dist/elisp")
  :after (lambda () 
           (require 'ensime)
+          ;; scala-mode can be found in the scala distribution:
+          (require 'scala-mode-auto)
           (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)))
