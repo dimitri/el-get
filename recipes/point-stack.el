4b9483904cc5bd44e1a0171416c5925436ddb470
Christian Köstlin <christian.koestlin@gmail.com>
2010-11-03 17:05:10 +0800

    point-stack added

---
+++ recipes/point-stack.el
@@ -0,0 +1,10 @@
+(:name point-stack
+       :type git
+       :url "git://github.com/mattharrison/point-stack.git"
+       :features point-stack
+       :compile "point-stack.el"
+       :after (lambda()
+		(global-set-key '[(f5)] 'point-stack-push)
+		(global-set-key '[(f6)] 'point-stack-pop)
+		(global-set-key '[(f7)] 'point-stack-forward-stack-pop)
+		))
