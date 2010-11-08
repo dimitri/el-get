1cde6493bce1b367e5c024d0228c6fbec81b27a9
Dimitri Fontaine <dim@tapoueh.org>
2010-11-08 13:52:25 +0100

    Remove the global-set-key setup from the point-stack recipe, keep it in comments.

--- recipes/point-stack.el
+++ recipes/point-stack.el
@@ -2,9 +2,12 @@
        :type git
        :url "git://github.com/mattharrison/point-stack.git"
        :features point-stack
-       :compile "point-stack.el"
-       :after (lambda()
-		(global-set-key '[(f5)] 'point-stack-push)
-		(global-set-key '[(f6)] 'point-stack-pop)
-		(global-set-key '[(f7)] 'point-stack-forward-stack-pop)
-		))
+       :compile "point-stack.el")
+
+;;
+;; Configuration example:
+;;
+;;        :after (lambda()
+;;        		(global-set-key '[(f5)] 'point-stack-push)
+;;        		(global-set-key '[(f6)] 'point-stack-pop)
+;;        		(global-set-key '[(f7)] 'point-stack-forward-stack-pop))
