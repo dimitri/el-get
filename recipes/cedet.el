89824f5960ddb252329b0769bb6ec188f6f58322
Alex Ott <alexott@gmail.com>
2010-11-29 14:33:11 +0100

    provide actual information about CEDET

--- recipes/cedet.el
+++ recipes/cedet.el
@@ -1,6 +1,5 @@
 (:name cedet
-  :type cvs
-  :module "cedet"
-  :url ":pserver:anonymous@cedet.cvs.sourceforge.net:/cvsroot/cedet"
+  :type bzr
+  :url "bzr://cedet.bzr.sourceforge.net/bzrroot/cedet/code/trunk"
   :build ("touch `find . -name Makefile`" "make")
   :load-path ("./common"))
