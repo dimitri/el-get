4358d1c5c6ba099927d1c0b6f5743cea867ccb17
Dave Abrahams <dave@boostpro.com>
2010-12-07 06:21:56 -0500

    Avoid shadowing the real el-get-dir variable, or the defvar won't stick

--- el-get-install.el
+++ el-get-install.el
@@ -17,16 +17,16 @@
 ;; So the idea is that you copy/paste this code into your *scratch* buffer,
 ;; hit C-j, and you have a working el-get.
 
-(let* ((el-get-dir        (expand-file-name "~/.emacs.d/el-get/"))
-       (dummy             (unless (file-directory-p el-get-dir)
-			    (make-directory el-get-dir t)))
+(let* ((el-get-dir-       (expand-file-name "~/.emacs.d/el-get/"))
+       (dummy             (unless (file-directory-p el-get-dir-)
+			    (make-directory el-get-dir- t)))
        (package           "el-get")
        (bname             "*el-get bootstrap*")
-       (pdir              (concat (file-name-as-directory el-get-dir) package))
+       (pdir              (concat (file-name-as-directory el-get-dir-) package))
        (git               (or (executable-find "git") (error "Unable to find `git'")))
        (url               "git://github.com/dabrahams/el-get.git")
        (el-get-sources    `((:name ,package :type "git" :url ,url :features el-get :compile "el-get.el")))
-       (default-directory el-get-dir)
+       (default-directory el-get-dir-)
        (process-connection-type nil) ; pipe, no pty (--no-progress)
        (status            (call-process git nil bname t "--no-pager" "clone" "-v" url package)))
   (set-window-buffer (selected-window) bname)
