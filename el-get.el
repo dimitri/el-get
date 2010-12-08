c25e34be1283b42ab70cdd9f67064d8f7c05989d
Dave Abrahams <dave@boostpro.com>
2010-12-08 12:24:15 -0500

    functions returning directories should return them as directory names, not file names

--- el-get.el
+++ el-get.el
@@ -407,7 +407,7 @@ given method."
 
 (defun el-get-package-directory (package)
   "Returns the package installation directory absolute name."
-  (concat (file-name-as-directory el-get-dir) package))
+  (file-name-as-directory (concat (file-name-as-directory el-get-dir) package)))
 
 (defun el-get-add-path-to-list (package list path)
   "(add-to-list LIST PATH) checking for path existence within
