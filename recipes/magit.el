4f713bf3974e7f4bf94b6d079600c73c53edf499
Dimitri Fontaine <dim@tapoueh.org>
2010-12-13 09:59:39 +0100

    Fix stupid typo on recipe for magit, preventing the feature to get required.

--- recipes/magit.el
+++ recipes/magit.el
@@ -4,5 +4,5 @@
        :info "."
        ;; that used to be how to build it :build ("./autogen.sh" "./configure" "make")
        :build ("make all")
-       :build/darwin `(,(concat "PATH=" invocation-directory ":$PATH make all")))
+       :build/darwin `(,(concat "PATH=" invocation-directory ":$PATH make all"))
        :features magit)
