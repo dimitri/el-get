ee6368a7bcbc61744a85d3447857ec99213a459c
Jonathan Marchand <jonathlela@gmail.com>
2010-12-20 21:41:35 +0800

    Add uim-el recipe

---
+++ recipes/uim-el.el
@@ -0,0 +1,18 @@
+(:name uim-el
+       :type svn
+       :url "http://uim.googlecode.com/svn/trunk/"
+       :load-path ("./emacs")
+       :build ("LC_MESSAGES=C ./make-wc.sh --prefix=`pwd`/build\
+               --disable-gnome-applet --disable-fep --without-gtk2" 
+	       "make"
+	       "make install")
+       :after (lambda () 
+		(setq uim-el-agent (concat 
+				    (file-name-as-directory
+				     (el-get-package-directory "uim-el"))
+				    "emacs/uim-el-agent"))
+		(setq uim-el-helper-agent (concat 
+				    (file-name-as-directory
+				     (el-get-package-directory "uim-el"))
+				    "emacs/uim-el-helper-agent"))))
+
