06e6af534c56b462f96a3867b90a6d43fbc5927e
Kanak Kshetri <kanakkshetri@fastmail.fm>
2010-12-22 01:49:36 +0800

    Tuareg-mode: Use emacsmirror which is more up-to-date instead of ocamlforge. Also, associate with ml files.

--- recipes/tuareg-mode.el
+++ recipes/tuareg-mode.el
@@ -1,4 +1,11 @@
 (:name tuareg-mode
-       :type bzr
-       :url "https://forge.ocamlcore.org/anonscm/bzr/tuareg/mode/trunk"
-       :features tuareg)
+       :type git
+       :url "git://github.com/emacsmirror/tuareg.git"
+       :load-path (".")
+       :after
+       (lambda ()
+         (add-to-list 'auto-mode-alist '("\\.ml[iylp]?" . tuareg-mode))
+         (autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+         (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
+         (dolist (ext '(".cmo" ".cmx" ".cma" ".cmxa" ".cmi"))
+           (add-to-list 'completion-ignored-extensions ext))))
