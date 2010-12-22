dddfb70695a0b37f3cfcc70e338645fecbd8498a
Kanak Kshetri <kanak@vboxie.(none)>
2010-12-22 07:37:32 -0500

    Geiser: Fix typo that breaks install.

--- recipes/geiser.el
+++ recipes/geiser.el
@@ -2,7 +2,7 @@
        :type git
        :url "git://git.sv.gnu.org/geiser.git"
        :load-path ("./elisp")
-       :build ("./autogen.sh" "./configure" "make" "cd doc ; ginstall-info --dir-file=./dir *.info")
+       :build ("./autogen.sh" "./configure" "make" "cd doc ; install-info --dir-file=./dir *.info")
        :info "doc"
        :features geiser-load
        )
