b1ed33155adf64d37efc5e5b17843eab4f5256d2
Kanak Kshetri <kanakkshetri@fastmail.fm>
2010-12-26 05:34:44 -0500

    Smex: added initialization commands to the recipe

--- recipes/smex.el
+++ recipes/smex.el
@@ -1,4 +1,9 @@
 (:name smex
        :type git
-       :url "http://github.com/nonsequitur/smex.git")
+       :url "http://github.com/nonsequitur/smex.git"
+       :features smex
+       :after (lambda ()
+                (smex-initialize)))
+
+
 
