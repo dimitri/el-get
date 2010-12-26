315405a736cf35a4c0121b955d0a035f8da3ec86
Kanak Kshetri <kanakkshetri@fastmail.fm>
2010-12-26 23:37:35 +0800

    Python-related recipes.
    Added recipe for ipython.el
    Fixed initialization of pylookup and python-mode.

--- recipes/pylookup.el
+++ recipes/pylookup.el
@@ -1,3 +1,10 @@
 (:name pylookup
        :type git
-       :url "git://github.com/tsgates/pylookup.git")
+       :url "git://github.com/tsgates/pylookup.git"
+       :feature pylookup
+       :after (lambda ()
+                (setq pylookup-dir (concat el-get-dir "pylookup.py"))
+                (setq pylookup-program (concat pylookup-dir "pylookup.py"))
+                (setq pylookup-db (concat pylookup-dir "pylookup.db"))
+                (autoload 'pylookup "pylookup")
+                (autoload 'pylookup-update "pylookup")))
