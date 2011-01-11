974d28b2924a8a134775806140cc5d2de72bddfa
Dimitri Fontaine <dim@tapoueh.org>
2011-01-11 13:38:00 +0100

    Add auto-mode-alist support for php-mode-improved

--- recipes/php-mode-improved.el
+++ recipes/php-mode-improved.el
@@ -1,4 +1,8 @@
 (:name php-mode-improved
        :type emacswiki
        :load ("php-mode-improved.el")
-       :features php-mode)
+       :features php-mode
+       :after (lambda ()
+		(dolist (ext '(php php3 php4 php5 phtml))
+		  (add-to-list auto-mode-alist '((format "\\.%s$" ext) .php-mode)))
+		(autoload 'python-mode "python-mode" "Python editing mode." t)))
