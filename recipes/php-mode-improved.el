429e5ae3cd913e5620f05c20747a043011e888f2
Dimitri Fontaine <dim@tapoueh.org>
2011-01-11 15:46:25 +0100

    It so happens that I was overzealous.

--- recipes/php-mode-improved.el
+++ recipes/php-mode-improved.el
@@ -1,8 +1,4 @@
 (:name php-mode-improved
        :type emacswiki
        :load ("php-mode-improved.el")
-       :features php-mode
-       :after (lambda ()
-		(dolist (ext '(php php3 php4 php5 phtml))
-		  (add-to-list auto-mode-alist '((format "\\.%s$" ext) .php-mode)))
-		(autoload 'php-mode "php-mode" "PHP improved editing mode." t)))
+       :features php-mode)
