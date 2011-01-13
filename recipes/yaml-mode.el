0d1af3f87c1c26d4d0b8a4f30837c84c4bdc59d8
Victor Deryagin <vderyagin@gmail.com>
2011-01-13 17:03:57 +0800

    improved js2-mode, textile-mode, yaml-mode recipes (added autoloads, etc)

--- recipes/yaml-mode.el
+++ recipes/yaml-mode.el
@@ -1,7 +1,6 @@
 (:name yaml-mode
        :type git
        :url "git://github.com/yoshiki/yaml-mode.git"
-       :features yaml-mode
        :after (lambda ()
-                (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
-                (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))))
+                (autoload 'yaml-mode "yaml-mode" nil t)
+                (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-mode))))
