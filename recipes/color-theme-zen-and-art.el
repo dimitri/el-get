a20c26a2b37531339fdd2b127dbf8e49e84628bc
Dimitri Fontaine <dim@tapoueh.org>
2011-01-29 23:55:19 +0100

    Per issue #127, have color-theme-zen-and-art recipe activates itself.

--- recipes/color-theme-zen-and-art.el
+++ recipes/color-theme-zen-and-art.el
@@ -1,1 +1,4 @@
-(:name color-theme-zen-and-art :type git :url "git://github.com/irfn/zen-and-art.git")
+(:name color-theme-zen-and-art
+       :type git
+       :url "git://github.com/irfn/zen-and-art.git"
+       :after (lambda () (color-theme-zen-and-art)))
