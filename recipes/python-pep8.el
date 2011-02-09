93c17a60f623682d77d39fb4590907dbabec3cbc
Jonas Baumann <jone@jone.ch>
2011-02-09 22:00:48 +0100

    added python-pep8 recipe

---
+++ recipes/python-pep8.el
@@ -0,0 +1,6 @@
+(:name python-pep8
+       :type http
+       :url "https://gist.github.com/raw/302847/3331473995b55cc578e7d63dd82474749367c29c/python-pep8.el"
+       :features python-pep8
+       :after (lambda ()
+                (require 'tramp)))
