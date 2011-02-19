00a8814d52077c492cd0809024d315b9a6141e55
Dimitri Fontaine <dim@tapoueh.org>
2011-02-19 22:06:03 +0100

    Test for already installed el-get in el-get-install.el rather than in the doc snippet.

--- el-get-install.el
+++ el-get-install.el
@@ -17,29 +17,31 @@
 ;; So the idea is that you copy/paste this code into your *scratch* buffer,
 ;; hit C-j, and you have a working el-get.
 
-(let* ((el-get-root
-	(file-name-as-directory
-	 (concat (file-name-as-directory user-emacs-directory) "el-get")))
-       (dummy             (unless (file-directory-p el-get-root)
-			    (make-directory el-get-root t)))
-       (package           "el-get")
-       (buf               (switch-to-buffer "*el-get bootstrap*"))
-       (pdir              (file-name-as-directory (concat el-get-root package)))
-       (git               (or (executable-find "git")
-			      (error "Unable to find `git'")))
-       (url               "git://github.com/dimitri/el-get.git")
-       (default-directory el-get-root)
-       (process-connection-type nil) ; pipe, no pty (--no-progress)
+(if (require 'el-get nil t)
+    (message "el-get is already installed, try M-x el-get-update")
+  (let* ((el-get-root
+	  (file-name-as-directory
+	   (concat (file-name-as-directory user-emacs-directory) "el-get")))
+	 (dummy             (unless (file-directory-p el-get-root)
+			      (make-directory el-get-root t)))
+	 (package           "el-get")
+	 (buf               (switch-to-buffer "*el-get bootstrap*"))
+	 (pdir              (file-name-as-directory (concat el-get-root package)))
+	 (git               (or (executable-find "git")
+				(error "Unable to find `git'")))
+	 (url               "git://github.com/dimitri/el-get.git")
+	 (default-directory el-get-root)
+	 (process-connection-type nil) ; pipe, no pty (--no-progress)
 
-       ;; First clone el-get
-       (status
-        (call-process git nil `(,buf t) t "--no-pager" "clone" "-v" url package)))
+	 ;; First clone el-get
+	 (status
+	  (call-process git nil `(,buf t) t "--no-pager" "clone" "-v" url package)))
 
-  (unless (zerop status)
-    (error "Couldn't get el-get from the Git repository"))
+    (unless (zerop status)
+      (error "Couldn't get el-get from the Git repository"))
 
-  (load (concat pdir package ".el"))
-  (el-get-post-install "el-get" 'noerror)
-  (with-current-buffer buf
-    (goto-char (point-max))
-    (insert "\nCongrats, el-get is installed and ready to serve!")))
+    (load (concat pdir package ".el"))
+    (el-get-post-install "el-get" 'noerror)
+    (with-current-buffer buf
+      (goto-char (point-max))
+      (insert "\nCongrats, el-get is installed and ready to serve!"))))
