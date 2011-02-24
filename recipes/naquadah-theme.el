eafaad7087c9d3b47e10364fa82abf6b76cc2bbc
Julien Danjou <julien@danjou.info>
2011-02-24 23:50:43 +0100

    naquadah-theme: remove broken post-init
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- recipes/naquadah-theme.el
+++ recipes/naquadah-theme.el
@@ -1,5 +1,3 @@
 (:name naquadah-theme
        :type git
-       :url "git://git.naquadah.org/naquadah-theme.git"
-       :post-init (lambda ()
-		    (add-to-list 'custom-theme-load-path pdir)))
+       :url "git://git.naquadah.org/naquadah-theme.git")
