92aeaa19be2be8e381652445d09b6d5cb3aa8afe
Mat Marcus <mmarcus@emarcus.org>
2011-03-06 08:11:53 +0800

    update nxhtml recipe to run nxhtmlmaint-start-byte-compile for compilation

--- recipes/nxhtml.el
+++ recipes/nxhtml.el
@@ -1,4 +1,6 @@
 (:name nxhtml
        :type git
+       :build
+         (list (concat el-get-emacs " -batch -q -no-site-file -L . -l nxhtmlmaint.el -f nxhtmlmaint-start-byte-compilation"))
        :url "http://github.com/emacsmirror/nxhtml.git"
        :load "autostart.el")
