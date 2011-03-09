ea3394ce62e71719432eaf0f7f22c9bf57f05a32
Dimitri Fontaine <dim@tapoueh.org>
2011-03-09 15:25:07 +0100

    crontab-mode is to be fetched on web.archive.org now

--- recipes/crontab-mode.el
+++ recipes/crontab-mode.el
@@ -1,1 +1,5 @@
-(:name crontab-mode :type http :url "http://www.mahalito.net/~harley/elisp/crontab-mode.el")
+(:name crontab-mode
+       :type http
+       :url "http://web.archive.org/web/20080716014153/http://www.mahalito.net/~harley/elisp/crontab-mode.el")
+
+       ;; :url "http://www.mahalito.net/~harley/elisp/crontab-mode.el")
