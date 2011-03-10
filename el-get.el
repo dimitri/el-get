75d4c30fdaa12f827c498df7fe30e963cddb304d
Dimitri Fontaine <dim@tapoueh.org>
2011-03-10 21:15:29 +0100

    Fix some byte compile warnings.

--- el-get.el
+++ el-get.el
@@ -1248,7 +1248,8 @@ password prompt."
 (defun el-get-fink-install (package url post-install-fun)
   "sudo -S fink install PACKAGE"
   (let* ((name (format "*fink install %s*" package))
-         (pkgname (or (plist-get source :pkgname) package))
+	 (source  (el-get-package-def package))
+	 (pkgname (or (plist-get source :pkgname) package))
 	 (ok   (format "Package %s installed." package))
 	 (ko   (format "Could not install package %s." package)))
 
@@ -1268,6 +1269,7 @@ password prompt."
 (defun el-get-fink-remove (package url post-remove-fun)
   "apt-get remove PACKAGE. URL is there for API compliance."
   (let* ((name (format "*fink remove %s*" package))
+	 (source  (el-get-package-def package))
          (pkgname (or (plist-get source :pkgname) package))
 	 (ok   (format "Package %s removed." package))
 	 (ko   (format "Could not remove package %s." package)))
@@ -1694,7 +1696,7 @@ avoid doing it all over again"
             ((debug error) ;; catch-all, allow for debugging
              (message "%S" (error-message-string err)))))))))
 
-(defun el-get-byte-compile-files (&rest files)
+(defun el-get-byte-compile-files (pdir &rest files)
   "byte-compile the files or directories FILES.
 
 FILES files will get byte compiled if there's no .elc or the
@@ -1759,7 +1761,7 @@ directories."
 	    (push dir files))))
       ;; now that we have the list
       (when files
-	(apply 'el-get-byte-compile-files (nreverse files))))))
+	(apply 'el-get-byte-compile-files pdir (nreverse files))))))
 
 (defun el-get-build-commands (package)
   "Return a list of build commands for the named PACKAGE.
