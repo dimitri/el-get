5945fe213615d4c73493c936c822206723f3a8f6
Julien Danjou <julien@danjou.info>
2011-03-10 11:57:05 +0100

    Fix gnuplot-mode recipe
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- recipes/gnuplot-mode.el
+++ recipes/gnuplot-mode.el
@@ -1,1 +1,6 @@
-(:name gnuplot-mode :type http :url "http://cars9.uchicago.edu/~ravel/software/gnuplot-mode/gnuplot-mode.0.6.0.tar.gz")
+(:name gnuplot-mode
+       :type http-tar
+       :url "http://cars9.uchicago.edu/~ravel/software/gnuplot-mode/gnuplot-mode.0.6.0.tar.gz"
+       :options ("xzf")
+       :build ("./configure" "make")
+       :info "gnuplot.info")
