2465cef4dc8950640a86d17c4b62475d2e636654
Nodir Gulyamov <gelvaos@gmail.com>
2011-03-14 02:05:56 +0800

    Fixed the following recipes for building in windows (requires cygwin bin in PATH):   - cedet.el   - dvc.el   - emacs-w3m.el   - geiser.el

--- recipes/cedet.el
+++ recipes/cedet.el
@@ -2,4 +2,7 @@
   :type bzr
   :url "bzr://cedet.bzr.sourceforge.net/bzrroot/cedet/code/trunk"
   :build ("touch `find . -name Makefile`" "make")
+  :build/windows-nt ("echo #!/bin/sh > tmp.sh & echo touch `/usr/bin/find . -name Makefile` >> tmp.sh & echo make FIND=/usr/bin/find >> tmp.sh" 
+		     "sed 's/^M$//' tmp.sh  > tmp2.sh"
+		     "sh ./tmp2.sh" "rm ./tmp.sh ./tmp2.sh")
   :load-path ("./common"))
