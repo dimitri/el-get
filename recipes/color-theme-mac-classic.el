eb8c5d7fb1680a81df6f47e88dcd38390ea09959
Pásztor János <model87@freemail.hu>
2011-03-19 09:31:11 +0100

    Remove the autoload from all color-themes. It is annoying if you try to get all of them, you will get a flashing start.

--- recipes/color-theme-mac-classic.el
+++ recipes/color-theme-mac-classic.el
@@ -1,5 +1,4 @@
 (:name color-theme-mac-classic
        :type git
        :url "https://github.com/jbw/color-theme-mac-classic.git"
-       :features color-theme-mac-classic
-       :post-init (lambda () (color-theme-mac-classic)))
+       :features color-theme-mac-classic)
