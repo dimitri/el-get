29f2fdaaf99edc3b003ee3e0ae7d2f29c8c2b269
Ryan Edwards <ryan@ryangedwards.com>
2011-04-27 01:01:15 -0700

    Modified Solarized color theme to use the GitHub repository the developer of the Emacs port uses. The main Solarized repo lags being this one by some time.

--- recipes/color-theme-solarized.el
+++ recipes/color-theme-solarized.el
@@ -1,4 +1,4 @@
 (:name color-theme-solarized
-       :type http
-       :url "https://gist.github.com/raw/888286/7bb905279b1af2c1bec525e71a4f0cca8948c90a/color-theme-solarized.el"
-       :load "color-theme-solarized.el")
+       :type git
+       :url "https://github.com/sellout/emacs-color-theme-solarized.git"
+       :features color-theme-solarized)
