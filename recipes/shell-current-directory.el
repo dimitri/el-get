868c94988973551f9dca2457d9810751c9c05ad9
Terrence Brannon <metaperl@gmail.com>
2011-05-19 18:00:09 -0400

    force (require 'shell-current-directory)

--- recipes/shell-current-directory.el
+++ recipes/shell-current-directory.el
@@ -1,5 +1,7 @@
 (:name shell-current-directory
        :type git 
-       :url "https://github.com/metaperl/shell-current-directory.git")
+       :url "https://github.com/metaperl/shell-current-directory.git"
+       :features shell-current-directory
+       )
 
 
