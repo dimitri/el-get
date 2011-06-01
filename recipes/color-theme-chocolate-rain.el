10fe3ca89e3858e5bdf47ab1067226f4280844c4
Dave Abrahams <dave@boostpro.com>
2011-06-01 19:22:25 -0400

    Add dependencies for color themes

--- recipes/color-theme-chocolate-rain.el
+++ recipes/color-theme-chocolate-rain.el
@@ -1,3 +1,4 @@
 (:name color-theme-chocolate-rain
        :type git
-       :url "https://github.com/marktran/color-theme-chocolate-rain.git")
+       :url "https://github.com/marktran/color-theme-chocolate-rain.git"
+       :depends color-theme)
--- recipes/color-theme-desert.el
+++ recipes/color-theme-desert.el
@@ -1,3 +1,4 @@
 (:name color-theme-desert
        :type git
-       :url "https://github.com/superbobry/color-theme-desert.git")
+       :url "https://github.com/superbobry/color-theme-desert.git"
+       :depends color-theme)
