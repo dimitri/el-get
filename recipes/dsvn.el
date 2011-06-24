55a2bc632d6a594562efc330b88e8b306a8440c2
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2011-06-24 19:44:24 +0200

    Added even more :website/:description properties.

--- recipes/dsvn.el
+++ recipes/dsvn.el
@@ -1,1 +1,3 @@
-(:name dsvn :type http :url "http://svn.apache.org/repos/asf/subversion/trunk/contrib/client-side/emacs/dsvn.el")
+(:name dsvn
+       :description "This is an interface for managing Subversion working copies.  It can show you an up-to-date view of the current status, and commit changes. If also helps you do other tasks such as updating, switching, diffing and more."
+       :type http :url "http://svn.apache.org/repos/asf/subversion/trunk/contrib/client-side/emacs/dsvn.el")
