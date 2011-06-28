4b1650a4176f742dbcc841a5a01b4e03ca490538
Dimitri Fontaine <dim@tapoueh.org>
2011-06-28 21:21:34 +0200

    Add recipe for muse-website.

---
+++ recipes/muse-website.el
@@ -0,0 +1,5 @@
+(:name muse-website
+       :website "https://github.com/renard/muse-website"
+       :description "Generate a website from Muse"
+       :type git
+       :url "https://github.com/renard/muse-website.git")
