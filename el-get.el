ece7d19c3ef051ac0c996dc6bd26a6f08b5e6e1e
Dave Abrahams <dave@boostpro.com>
2011-07-05 19:27:40 -0400

    Move "deprecated" el-get-sources elements to the end

--- el-get.el
+++ el-get.el
@@ -849,16 +849,10 @@ this is the name to fetch in that system"
        (group :inline t :format "CVS Module: %v"
               (const :format "" :module)
               (string :format "%v"))
-       (group :inline t
-              :format "`Before' Function (`Prepare' recommended instead): %v"
-              (const :format "" :before) (function :format "%v"))
        (group :inline t :format "`Prepare' Function: %v"
               (const :format "" :prepare) (function :format "%v"))
        (group :inline t :format "`Post-Init' Function: %v"
               (const :format "" :post-init) (function :format "%v"))
-       (group :inline t
-              :format "`After' Function (`Post-Init' recommended instead): %v"
-              (const :format "" :after) (function :format "%v"))
        (group :inline t
               :format "Name of downloaded file (`http' and `ftp' only): %v"
               (const :format "" :localname) (string :format "%v"))
@@ -869,7 +863,13 @@ this is the name to fetch in that system"
               (const :format "" :repo)
               (cons :format "\n%v"
                     (string :tag "Name")
-                    (string :tag "URL"))))
+                    (string :tag "URL")))
+       (group :inline t
+              :format "`Before' Function (`Prepare' recommended instead): %v"
+              (const :format "" :before) (function :format "%v"))
+       (group :inline t
+              :format "`After' Function (`Post-Init' recommended instead): %v"
+              (const :format "" :after) (function :format "%v")))
       (repeat
        :inline t :tag "System-Specific Build Recipes"
        (group :inline t
