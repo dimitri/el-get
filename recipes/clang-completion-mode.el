ec9acb2607a22b582f63b32d8f098a0a0ecf0cd8
ChandleWEi <chandle.wei@gmail.com>
2011-07-19 10:42:09 +0800

    add llvm clang mode

---
+++ recipes/clang-completion-mode.el
@@ -0,0 +1,8 @@
+(:name clang-completion-mode
+       :type http
+       :url "https://llvm.org/svn/llvm-project/cfe/trunk/utils/clang-completion-mode.el"
+       ;;:info "session management"
+       ;; :autoloads nil
+       )
+
+
