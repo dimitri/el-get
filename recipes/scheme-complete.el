af9751bfc80f4ffd5e5c2aabca341ece81d4f81f
Drew Hess <dhess-src@bothan.net>
2011-07-26 13:33:09 -0700

    Add :description and :website to scheme-complete.

--- recipes/scheme-complete.el
+++ recipes/scheme-complete.el
@@ -1,4 +1,6 @@
 (:name scheme-complete
+       :website "http://synthcode.com/wiki/scheme-complete"
+       :description "Context-sensitive completion for Scheme code."
        :type http
        :url "http://synthcode.com/emacs/scheme-complete-0.8.7.el.gz"
        :build ("gunzip -c scheme-complete-0.8.7.el.gz > scheme-complete.el")
