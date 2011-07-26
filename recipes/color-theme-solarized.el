3a5e8517483dfc55ff4c5822154952f9496cb1e8
Drew Hess <dhess-src@bothan.net>
2011-07-26 14:28:21 -0700

    color-theme-solarized depends on color-theme.

--- recipes/color-theme-solarized.el
+++ recipes/color-theme-solarized.el
@@ -1,4 +1,5 @@
 (:name color-theme-solarized
        :type git
        :url "https://github.com/sellout/emacs-color-theme-solarized.git"
-       :load "color-theme-solarized.el")
+       :load "color-theme-solarized.el"
+       :depends color-theme)
