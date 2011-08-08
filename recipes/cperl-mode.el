f8bb6a678d15195edb77eea1224c9d22aae53b90
Nikolai Sevostjanov <nikolai.sevostjanov@gmail.com>
2011-08-08 17:40:03 +0800

    added cperl-mode recipe

---
+++ recipes/cperl-mode.el
@@ -0,0 +1,8 @@
+(:name cperl-mode
+       :website "https://github.com/jrockway/cperl-mode"
+       :description "Perl code editing commands for Emacs"
+       :type git
+       :url "https://github.com/jrockway/cperl-mode.git"
+       :depends mode-compile
+       :compile "cperl-mode.el"
+       :provide cperl-mode)
