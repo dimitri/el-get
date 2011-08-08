3d4b78f4b67b91c8b11f62422f89d034f61219bb
Nikolai Sevostjanov <nikolai.sevostjanov@gmail.com>
2011-08-08 12:05:08 +0300

    recipes for epkg

---
+++ recipes/listing.el
@@ -0,0 +1,7 @@
+(:name listing
+       :website "https://github.com/tarsius/listing"
+       :description "Show columnized view of list elements"
+       :type git
+       :url "https://github.com/tarsius/listing.git"
+       :depends (header-button map-progress)
+       :compile "listing.el")
