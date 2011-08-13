49f7ea7a271a9c8e6276036570d90363f4700304
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-14 00:31:17 +0900

    new recipe: ncl mode ncar command line language

---
+++ recipes/ncl.el
@@ -0,0 +1,6 @@
+(:name ncl
+       :type git
+       :website "http://www.ncl.ucar.edu/"
+       :description "ncl-mode for ncar command line language"
+       :url "git://github.com/yyr/ncl.el.git"
+       :autoloads t)
