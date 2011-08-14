78c991a56b2bf4fbf140e66020c8bed945d0473f
Dimitri Fontaine <dim@tapoueh.org>
2011-08-14 16:03:45 +0200

    Fix gnuplot-mode recipe, per issue #255

--- recipes/gnuplot-mode.el
+++ recipes/gnuplot-mode.el
@@ -2,5 +2,6 @@
        :type http-tar
        :url "http://cars9.uchicago.edu/~ravel/software/gnuplot-mode/gnuplot-mode.0.6.0.tar.gz"
        :options ("xzf")
-       :build `("./configure" ,(concat "make EMACS=" el-get-emacs))
+       :build `("./configure"
+		,(concat "make EMACS=" el-get-emacs " gnuplot.elc gnuplot-gui.elc"))
        :info "gnuplot.info")
