d3630d8580bc66dd0c4695c40d8126d7997f1757
Dimitri Fontaine <dim@tapoueh.org>
2011-08-15 23:24:07 +0200

    Fix up a description for emacs-grads, and switch 2 new recipes to http protocol rather than git://

--- recipes/emacs-grads.el
+++ recipes/emacs-grads.el
@@ -1,5 +1,6 @@
 (:name emacs-grads
-       :type git
-       :url "git://github.com/yyr/emacs-grads.git"
+       :description "Grid Analysis and Display System (GrADS)"
        :website "http://www.iges.org/grads/grads.html"
+       :type git
+       :url "http://github.com/yyr/emacs-grads.git"
        :autoloads t)
--- recipes/epresent.el
+++ recipes/epresent.el
@@ -1,6 +1,6 @@
 (:name epresent
        :type git
-       :url "git://github.com/eschulte/epresent.git"
+       :url "http://github.com/eschulte/epresent.git"
        :description "make presentation slides from a org-mode file within emacs"
        :autoloads t)
 
