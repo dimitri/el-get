998c2a0faf309d96e91e31ff84304b89e40201a7
Dimitri Fontaine <dim@tapoueh.org>
2011-08-22 12:23:48 +0200

    Fix inproper usage of :info in recipes for pov-mode and session.

--- recipes/session.el
+++ recipes/session.el
@@ -4,6 +4,5 @@
        :options ("xzf")
        :load-path ("lisp")
        :url "http://downloads.sourceforge.net/project/emacs-session/session/2.2a/session-2.2a.tar.gz"
-       :info "session management"
        :autoloads nil
        )
