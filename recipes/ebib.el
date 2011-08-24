e6e9aea8683ab3289922a9bb53a9b31239786bc5
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-24 13:22:56 +0900

    fix ebib.el url

--- recipes/ebib.el
+++ recipes/ebib.el
@@ -2,5 +2,5 @@
        :type git
        :website "http://ebib.sourceforge.net/index.html"
        :description "Application to manage BibTeX database"
-       :url "http://repo.or.cz/w/ebib.git"
+       :url "http://repo.or.cz/r/ebib.git"
        :autoloads t)
