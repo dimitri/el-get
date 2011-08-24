b80bfc8a30a1946a7099da23834b654d9ee30fa2
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-24 13:09:53 +0900

    new recipe: ebib.el

---
+++ recipes/ebib.el
@@ -0,0 +1,6 @@
+(:name ebib
+       :type git
+       :website "http://ebib.sourceforge.net/index.html"
+       :description "Application to manage BibTeX database"
+       :url "http://repo.or.cz/w/ebib.git"
+       :autoloads t)
