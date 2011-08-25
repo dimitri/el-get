9db06aa34c838ceff60d4507bad347bcb50f1e52
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-25 19:28:09 +0900

    new recipe: dictem.el , fix path in ebib.el

--- recipes/ebib.el
+++ recipes/ebib.el
@@ -3,4 +3,5 @@
        :website "http://ebib.sourceforge.net/index.html"
        :description "Application to manage BibTeX database"
        :url "http://repo.or.cz/r/ebib.git"
+       :load-path ("src")
        :autoloads t)
