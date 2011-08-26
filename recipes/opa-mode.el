c133983191f88891addc3274b768141cae7e270e
L42y <423300@gmail.com>
2011-08-26 22:14:27 +0800

    opa mode

---
+++ recipes/opa-mode.el
@@ -0,0 +1,5 @@
+(:name opa-mode
+       :website "https://opalang.org"
+       :description "Major mode for editing OPA programs"
+       :type http
+       :url "https://github.com/MLstate/opalang/raw/master/utils/emacs/opa-mode.el")
