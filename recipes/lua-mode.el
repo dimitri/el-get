031cb11b9c3f41a888a5ae836bbcd8276d872b23
Rafael Sanchez <rafael@cornerdimension.com>
2011-08-30 21:46:38 -0300

    Backport of lua-mode recipe

--- recipes/lua-mode.el
+++ recipes/lua-mode.el
@@ -1,7 +1,4 @@
 (:name lua-mode
        :type git
-       :url "https://github.com/rrthomas/lua-mode.git"
-       :features lua-mode
-       :post-init (lambda ()
-		    (add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-mode))
-		    (autoload 'lua-mode "lua-mode" "Lua editing mode." t)))
+       :url "https://github.com/immerrr/lua-mode"
+       :features lua-mode)
