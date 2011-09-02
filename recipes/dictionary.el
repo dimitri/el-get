95fa8178dc5ef8cae291a3848d034da3cb916ad5
Sébastien Gross <sebchezwamorg>
2011-09-02 20:58:34 +0800

    Add recipe for dictionary.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

---
+++ recipes/dictionary.el
@@ -0,0 +1,7 @@
+(:name dictionary
+       :website "http://www.myrkr.in-berlin.de/dictionary/"
+       :description "Emacs package for talking to a dictionary server"
+       :type http-tar
+       :options ("xzf")
+       :url "http://www.myrkr.in-berlin.de/dictionary/dictionary-1.8.7.tar.gz"
+       :build ("make"))
