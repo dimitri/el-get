de24c9a33fe6a55a12652a7cc0e104696ffc88c5
Dimitri Fontaine <dim@tapoueh.org>
2011-09-17 22:18:19 +0300

    edit el-get-version in 3.stable branch

--- el-get.el
+++ el-get.el
@@ -153,7 +153,7 @@
 (defgroup el-get nil "el-get customization group"
   :group 'convenience)
 
-(defconst el-get-version "3.0" "el-get version number")
+(defconst el-get-version "3.1" "el-get version number")
 
 (defcustom el-get-post-init-hooks nil
   "Hooks to run after a package init.
