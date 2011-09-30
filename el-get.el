fe584fb4f1bd200b1c58d69abe235179ad954580
Dimitri Fontaine <dim@tapoueh.org>
2011-09-30 14:42:46 +0200

    Upgrade version number to 4.0.1, so that the package change happened in 4.0.1.

--- el-get.el
+++ el-get.el
@@ -148,7 +148,7 @@
 (defgroup el-get nil "el-get customization group"
   :group 'convenience)
 
-(defconst el-get-version "4.0.0" "el-get version number")
+(defconst el-get-version "4.0.1" "el-get version number")
 
 (defconst el-get-script (or load-file-name buffer-file-name))
 
