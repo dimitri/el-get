5b17953aa0f1fe4916821309aa39f031b0cd41e4
Dimitri Fontaine <dim@tapoueh.org>
2011-10-04 13:39:09 +0200

    Add the headers and licence to methods/el-get-builtin.el

--- methods/el-get-builtin.el
+++ methods/el-get-builtin.el
@@ -1,3 +1,17 @@
+;;; el-get --- Manage the external elisp bits and pieces you depend upon
+;;
+;; Copyright (C) 2010-2011 Dimitri Fontaine
+;;
+;; Author: Dimitri Fontaine <dim@tapoueh.org>
+;; URL: http://www.emacswiki.org/emacs/el-get
+;; GIT: https://github.com/dimitri/el-get
+;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; Install
+;;     Please see the README.asciidoc file from the same distribution
+
 (require 'el-get-core)
 
 (defcustom el-get-builtin-install-hook nil
