3fa180a61f307828ed6fafb347caae0e461b9537
Ryan C. Thompson <rct@thompsonclan.org>
2011-10-06 02:06:29 -0700

    Use a list of arguments instead of a single string for command
    The `el-get-install-or-init-info` composed a command as a single
    pre-quoted string using `concat` and `shell-quote-argument`. It is
    better to make the command a list of unquoted arguments, which is what
    this commit does.

--- el-get-build.el
+++ el-get-build.el
@@ -166,13 +166,12 @@ recursion.
 	     (el-get-set-info-path package infodir-rel)
 	     (el-get-build
 	      package
-	      `(,(format "%s %s dir"
-			 (shell-quote-argument el-get-install-info)
-			 (shell-quote-argument
-			  (if (string= (substring infofile -5) ".info")
+              (list (list el-get-install-info
+                          (if (string= (substring infofile -5) ".info")
 			      infofile
-			    (concat infofile ".info"))))) infodir-rel t nil t)))
-
+			    (concat infofile ".info"))
+                          "dir"))
+              infodir-rel t nil t)))
 	  (t
 	   (error
 	    "el-get-install-or-init-info: %s not supported" build-or-init)))))))
