03130507ab9400be91d7da6db74e3e6fd7ed0bcf
Dimitri Fontaine <dim@tapoueh.org>
2011-10-28 18:04:38 +0200

    Call it 4.0.6 now.

--- el-get.el
+++ el-get.el
@@ -158,7 +158,7 @@
 (defgroup el-get nil "el-get customization group"
   :group 'convenience)
 
-(defconst el-get-version "4.0.5" "el-get version number")
+(defconst el-get-version "4.0.6" "el-get version number")
 
 (defconst el-get-script (or load-file-name buffer-file-name))
 
