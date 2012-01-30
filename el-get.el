10b6175d4f06f7617d57a9ef07aaf7e1d180e555
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-29 21:14:23 -0800

    Add github & emacsmirror methods to Change Log

--- el-get.el
+++ el-get.el
@@ -41,6 +41,7 @@
 ;;   - fix recipes :build commands, must be either lists of strings or expr
 ;;   - add support for el-get-reload and do that at update time
 ;;   - implement :checksum property for http kinds of files
+;;   - New package types: github, emacsmirror
 ;;
 ;;  3.1 - 2011-09-15 - Get a fix
 ;;
