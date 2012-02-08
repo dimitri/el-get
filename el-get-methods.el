275004b5a8ec59dea2a92c4e441b0c8ac636d1a1
Ryan C. Thompson <rct@thompsonclan.org>
2012-02-08 10:16:03 -0800

    Add github-tar method

--- el-get-methods.el
+++ el-get-methods.el
@@ -38,6 +38,7 @@
 (require 'el-get-http)
 (require 'el-get-http-tar)
 (require 'el-get-http-zip)
+(require 'el-get-github-tar)
 (require 'el-get-pacman)
 (require 'el-get-svn)
 
---
+++ methods/el-get-github-tar.el
@@ -0,0 +1,42 @@
+;;; el-get --- Manage the external elisp bits and pieces you depend upon
+;;
+;; Copyright (C) 2010-2011 Dimitri Fontaine
+;;
+;; Author: Dimitri Fontaine <dim@tapoueh.org>
+;; URL: http://www.emacswiki.org/emacs/el-get
+;; GIT: https://github.com/dimitri/el-get
+;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; Install
+;;     Please see the README.asciidoc file from the same distribution
+
+(require 'el-get-http-tar)
+
+(defun el-get-github-tar-url (package)
+  (let* ((source (el-get-package-def package)))
+    (or
+     ;; Use :url if provided
+     (plist-get source :url)
+     ;; Else generate URL from username, and reponame
+     (let* ((username (el-get-as-string
+                       (or (plist-get source :username)
+                           (error "Recipe for Github tar package %s needs a username" package))))
+            (reponame (el-get-as-string
+                       (or (plist-get source :reponame)
+                           package))))
+       (format "https://github.com/%s/%s/tarball/master"
+               username reponame)))))
+
+(defun el-get-github-tar-install (package url post-install-fun)
+  "Clone the given package from Github following the URL."
+  (el-get-http-tar-install package
+                           (or url (el-get-github-tar-url package))
+                           post-install-fun))
+
+(el-get-register-derived-method :github-tar :http-tar
+  :install #'el-get-github-tar-install
+  :update #'el-get-github-tar-install)
+
+(provide 'el-get-github-tar)
