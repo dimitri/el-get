aaa09767561ef396c8f895c0b3646071af7cb6b3
Ryan C. Thompson <rct@thompsonclan.org>
2012-02-13 21:28:38 -0800

    Add test for issue 548

---
+++ test/el-get-issue-548.el
@@ -0,0 +1,13 @@
+;; https://github.com/dimitri/el-get/issues/548
+;;
+;; Use `default-directory' in :post-init and similar recipe init hooks.
+
+(let ((debug-on-error t)
+      ;; (el-get-byte-compile nil)
+      (el-get-verbose t))
+  (require 'el-get)
+  (el-get 'sync 'pcmpl-git)
+  (el-get-init 'pcmpl-git)
+  ;; Verify that the option in :post-init was set correctly
+  (assert (string= pcmpl-git-options-file
+                   (expand-file-name "git-options" (el-get-package-directory 'pcmpl-git)))))
