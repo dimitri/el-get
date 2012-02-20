bb29591fa9ed5780300ac622634dd47b5a555f0a
Ryan C. Thompson <rct@thompsonclan.org>
2012-02-20 15:00:59 -0800

    Support "user/repo" for pkgname in github-tar/zip recipes

--- methods/el-get-github-zip.el
+++ methods/el-get-github-zip.el
@@ -20,14 +20,21 @@
      ;; Use :url if provided
      (plist-get source :url)
      ;; Else generate URL from username, and reponame
-     (let* ((username (el-get-as-string
-                       (or (plist-get source :username)
-                           (error "Recipe for Github zip package %s needs a username" package))))
+     (let* ((username (plist-get source :username))
             (reponame (el-get-as-string
                        (or (plist-get source :pkgname)
                            package)))
             (branch (or (plist-get source :branch)
                         "master")))
+       ;; A slash in the repo name means that it is "user/repo"
+       (when (string-match-p "/" reponame)
+         (let* ((split (split-string reponame "[[:space:]]\\|/" 'omit-nulls)))
+           (assert (= (length split) 2) nil
+                   "Github pkgname %s must contain only one slash and no spaces" reponame)
+           (setq username (first split)
+                 reponame (second split))))
+       (unless username
+         (error "Recipe for Github-zip package %s needs a username" package))
        (format "https://github.com/%s/%s/zipball/%s"
                username reponame branch)))))
 
