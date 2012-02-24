c71491a417f2cc3d29b1b8beb68da06b42795c02
Ryan C. Thompson <rct@thompsonclan.org>
2012-02-24 12:03:45 -0800

    Add an explanatory "comment" to .dir-locals.el
    It is actually a string because "add-dir-local-variable" and related
    functions will blow away comments in the file.

--- .dir-locals.el
+++ .dir-locals.el
@@ -13,5 +13,6 @@
         (set-face-attribute 'whitespace-line nil :background "red1" :foreground "yellow" :weight 'bold)
         (set-face-attribute 'whitespace-tab nil :background "red1" :foreground "yellow" :weight 'bold)
         (require 'whitespace)
+        "Need to ensure that whitespace mode is turned off and on again. This guaranteees that the new values of the whitespace-related variables will take effect."
         (whitespace-mode 0)
         (whitespace-mode 1))))
