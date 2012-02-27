5e31f0a235d1bd5c790a395e3f3091551410834e
Ryan C. Thompson <rct@thompsonclan.org>
2012-02-27 11:41:12 -0800

    Test #407 now unloads el-get before running
    This ensures that it will always try to install el-get during the
    test.

--- test/el-get-issue-407.el
+++ test/el-get-issue-407.el
@@ -1,8 +1,27 @@
-;; https://github.com/dimitri/el-get/issues/400
+;; https://github.com/dimitri/el-get/issues/407
 ;;
 ;; use url-retrieve-synchronously in init file
 ;;
 ;; Drop the -L . option from the README.asciidoc for this very test.
+
+(require 'cl)
+;; Unload el-get and delete it from the load path
+(when (require 'el-get nil t)
+  ;; Remove from load-path
+  (setq load-path
+        (remove-if (apply-partially
+                    #'string-prefix-p
+                    (expand-file-name
+                     "."
+                     (file-name-directory
+                      (symbol-file 'el-get 'defun))))
+                   load-path))
+  ;; Unload features
+  (loop for feat in features
+        if (string-prefix-p "el-get" (symbol-name feat))
+        do (unload-feature feat 'force))
+  (assert (not (require 'el-get nil t)) nil
+          "el-get should not be loadable now"))
 
 (unless (require 'el-get nil t)
   (with-current-buffer
@@ -11,3 +30,6 @@
     (end-of-buffer)
     (eval-print-last-sexp))
   (message "el-get is ready now"))
+
+;; Make sure el-get is now available
+(require 'el-get)
