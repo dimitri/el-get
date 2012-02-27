2769d584f1cde74eb2c27c301243699d583483f1
Ryan C. Thompson <rct@thompsonclan.org>
2012-02-26 23:15:11 -0800

    Temporary fix for .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -8,11 +8,11 @@
  (emacs-lisp-mode
   (whitespace-style face trailing lines-tail)
   (whitespace-line-column . 80)
-  (eval progn
+  (eval ignore-errors
         (add-hook 'before-save-hook 'delete-trailing-whitespace)
+        (require 'whitespace)
         (set-face-attribute 'whitespace-line nil :background "red1" :foreground "yellow" :weight 'bold)
         (set-face-attribute 'whitespace-tab nil :background "red1" :foreground "yellow" :weight 'bold)
-        (require 'whitespace)
         "Need to ensure that whitespace mode is turned off and on again. This guaranteees that the new values of the whitespace-related variables will take effect."
         (whitespace-mode 0)
         (whitespace-mode 1))))
