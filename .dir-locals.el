0013603607d3c1836dd99246021df1918288222b
Ryan C. Thompson <rct@thompsonclan.org>
2012-03-02 15:53:09 -0800

    Add explanation for use of write-contents-functions

--- .dir-locals.el
+++ .dir-locals.el
@@ -9,6 +9,7 @@
   (whitespace-style face trailing lines-tail)
   (whitespace-line-column . 80)
   (eval ignore-errors
+        "Write-contents-functions is a buffer-local alternative to before-save-hook"
         (add-hook 'write-contents-functions
                   (lambda ()
                     (delete-trailing-whitespace (point-min) (point-max))
