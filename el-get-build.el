9aed2555f152768657a63acf08bbf9fe127a6366
Bryan Shell <github@ennor.co.uk>
2012-05-10 14:21:48 -0500

    Only install info when `el-get-install-info' is not nil.

--- el-get-build.el
+++ el-get-build.el
@@ -187,7 +187,7 @@ recursion.
 
 	  ((eq build-or-init 'build)
 	   ;; rebuild each time asked --- e.g. on update
-	   (when (and infodir
+	   (when (and infodir el-get-install-info
 		      (file-directory-p infodir-abs)
 		      (not (file-exists-p info-dir)))
 	     (el-get-set-info-path package infodir-rel)
