f42c023e10cf334306b0280c6ba90b10b9ec3500
Ryan C. Thompson <rct@thompsonclan.org>
2012-05-28 13:50:46 -0700

    Allow version to be specified as number, string, or list.
    See "el-get-version-to-list" for details.

--- test/el-get-issue-730.el
+++ test/el-get-issue-730.el
@@ -2,27 +2,27 @@
 ;;
 ;; el-get-remove needs to be more flexible
 
-(setq package-a-required-version 24
-      el-get-sources (list
-                      `(:name a
-                              :type builtin
-                              :minimum-emacs-version ,package-a-required-version))
+(setq versions-to-test (list 0 10 23 24 25 40 500 "24.1.50.1" "21.4" "500.2.3" '(23 3 50 1))
+      recipes-to-test (mapcar (lambda (version)
+                               `(:name a :type builtin :minimum-emacs-version ,version))
+                             versions-to-test)
       el-get-default-process-sync t)
 
 ;; Simulate same version, higher version, and lower version
-(loop for version in (list package-a-required-version
-                           (1+ package-a-required-version)
-                           (1- package-a-required-version))
+(loop for version in versions-to-test
       do
-      (let ((emacs-major-version version))
-        (if (>= emacs-major-version package-a-required-version)
-            ;; Package should install without error
+      (let* ((version-list (el-get-version-to-list version))
+             (should-install (not (version-list-< (version-to-list emacs-version)
+                                                  version-list)))
+             (el-get-sources
+              (list `(:name a :type builtin :minimum-emacs-version ,version))))
+        (message "Testing installing a package requiring version %S. Current emacs version is %s. Package install is expected to %s."
+                 version emacs-version (if should-install "succeed" "fail"))
+        (if should-install
             (progn
               (el-get-install 'a)
               (el-get-init 'a)
-              (el-get-remove 'a)
-              (message "Installing package with met emacs version succeeded."))
-          ;; Lower version: package should throw an error
+              (el-get-remove 'a))
           (condition-case err
               (progn
                 (el-get-install 'a)
