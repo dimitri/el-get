294704bf5841780373fa62eceb46e6559a9e25ee
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-25 01:56:49 +0100

    Add test/test-utils.sh

---
+++ test/test-utils.sh
@@ -0,0 +1,31 @@
+set_default () {
+  eval "
+if [ -z \$$1 ]; then
+  $1=$2
+fi
+"
+}
+
+# http://www.linuxjournal.com/content/use-bash-trap-statement-cleanup-temporary-files
+function on_exit()
+{
+    for i in "${on_exit_items[@]}"
+    do
+        eval $i
+    done
+}
+
+function add_on_exit()
+{
+    local n=${#on_exit_items[*]}
+    on_exit_items[$n]="$*"
+    if [[ $n -eq 0 ]]; then
+        trap on_exit EXIT
+    fi
+}
+
+set_default EL_GET_LIB_DIR "$(dirname "$(dirname "$(readlink -f "$0")")")"
+set_default TMPDIR "$(dirname "$(mktemp --dry-run)")"
+set_default TEST_HOME "$TMPDIR/el-get-test-home"
+set_default EMACS "$(which emacs)"
+set_default TEST_DIR "$(dirname $0)"
