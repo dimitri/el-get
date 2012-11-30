6e4622ec9a6145b2c66292ff548e3d13fc646869
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-30 22:23:01 +0100

    el-get-with-temp-home: new helper for tests
    Also suppress output for passing tests.

---
+++ test/pkgs/el-get-test-package.el
@@ -0,0 +1,8 @@
+;;; an elisp package for test.el to use to use
+
+(defun el-get-test-a-function () t)
+
+;;;###autoload
+(defun el-get-test-an-autoloaded-function () t)
+
+(provide 'el-get-test-package)
--- test/test.el
+++ test/test.el
@@ -7,7 +7,52 @@
     (defmacro* ert-deftest (name () &body docstring-keys-and-body)
       (message "Skipping tests, ERT is not available"))))
 
-(defconst testing-destination-dir "/tmp/emacs.d.testing")
+(defvar el-get-test-output-buffer nil)
+(when noninteractive
+  (defadvice message (around el-get-test-catch-output activate)
+    "redirect all `message' output to `el-get-test-output-buffer'."
+    (if el-get-test-output-buffer
+        (with-current-buffer el-get-test-output-buffer
+          (insert (apply #'format (ad-get-args 0)) "\n"))
+      ad-do-it)))
+
+(defconst el-get-test-files-dir
+  (file-name-directory
+   (or (eval-when-compile (bound-and-true-p byte-compile-file))
+       load-file-name
+       buffer-file-name)))
+
+(defmacro el-get-with-temp-home (&rest body)
+  "Evaluate BODY with a temporary `user-emacs-directory'.
+
+In batch mode, `message' output is suppressed unless there is an
+error.
+
+Following variables are bound to temporal values:
+* `user-emacs-directory'
+* `el-get-dir'
+* `el-get-status-file'
+* `el-get-autoload-file'"
+  `(let* ((user-emacs-directory
+           (make-temp-file "emacs.d.el-get-testing" 'dir))
+          (el-get-dir (mapconcat #'file-name-as-directory
+                                 `(,user-emacs-directory "el-get") ""))
+          (el-get-status-file (concat el-get-dir ".status.el"))
+          (el-get-autoload-file (concat el-get-dir ".loaddefs.el"))
+          (el-get-test-output-buffer
+           (when noninteractive (get-buffer-create "*el-get-test-output*"))))
+     (unwind-protect
+         (condition-case err
+             (progn
+               (make-directory el-get-dir t)
+               ,@body)
+           (error (when el-get-test-output-buffer
+                    (with-current-buffer el-get-test-output-buffer
+                      (princ (buffer-string))))
+                  (signal (car err) (cdr err))))
+       (delete-directory user-emacs-directory t)
+       (when el-get-test-output-buffer
+         (kill-buffer el-get-test-output-buffer)))))
 
 (ert-deftest el-get-recipe-dirs-test ()
   (let ((el-get-recipe-path
@@ -21,51 +66,33 @@
                          collect f)))))
 
 (ert-deftest el-get-trivial-install-test ()
-  (let* ((pkg 'el-get-trivial-install-test)
-         (pkg-name (symbol-name pkg))
-         (pkg-file (concat pkg-name ".el"))
-         (pkg-source (concat "/tmp/" pkg-file))
-         (user-emacs-directory testing-destination-dir)
-         (el-get-dir (concat (file-name-as-directory user-emacs-directory)
-                             "el-get"))
-         (el-get-status-file
-          (concat (file-name-as-directory el-get-dir) ".status.el"))
-         (pkg-destination-dir (mapconcat
-                               'file-name-as-directory
-                               (list user-emacs-directory "el-get" pkg-name)
-                               ""))
-         (pkg-destination (concat pkg-destination-dir pkg-file))
-         (el-get-sources `((:name ,pkg
-                                  :features (,pkg)
-                                  :type http
-                                  :url ,(concat "file://" pkg-source))))
-         (el-get-packages (mapcar 'el-get-source-name el-get-sources)))
-    (make-directory el-get-dir t)
-    (unwind-protect
-        (progn
-          (message "Checking %s is not loaded" pkg)
-          (should-not (featurep pkg))
-          (message "Creating %s package file in %s" pkg pkg-source)
-          (with-temp-file pkg-source
-            (insert (format "(defun %s-empty ())\n(provide '%s)\n" pkg pkg)))
-          (message "Verifying package file %s" pkg-source)
-          (should (file-exists-p pkg-source))
-          ;; and it's not empty
-          (should-not (zerop (nth 7 (file-attributes pkg-source))))
-          (message "Installing %s" pkg)
-          (should (progn
-                    (el-get 'sync el-get-packages)
-                    t))
-          (message "Verifying installed package file %s" pkg-destination)
-          (should (file-exists-p pkg-destination))
-          (should-not (zerop (nth 7 (file-attributes pkg-destination))))
-          (message "Verifying package %s was loaded" pkg)
-          (should (featurep pkg))
-          (message "Unloading and removing package %s" pkg)
-          (el-get-remove (symbol-name pkg))
-          (message "Verifying package %s was unloaded and removed" pkg)
-          (should-not (file-exists-p pkg-destination)))
-      (delete-file pkg-source))))
+  (el-get-with-temp-home
+   (let* ((pkg 'el-get-test-package)
+          (pkg-name (symbol-name pkg))
+          (pkg-file (concat pkg-name ".el"))
+          (pkg-source (expand-file-name
+                       pkg-file (concat el-get-test-files-dir "pkgs")))
+          (pkg-destination (expand-file-name
+                            pkg-file (concat el-get-dir pkg-name)))
+          (el-get-sources `((:name ,pkg
+                                   :features (,pkg)
+                                   :type http
+                                   :url ,(concat "file://" pkg-source)))))
+     (unwind-protect
+         (progn
+           (message "Checking %s is not loaded" pkg)
+           (should-not (featurep pkg))
+           (message "Installing %s" pkg)
+           (should (progn (el-get 'sync pkg) t))
+           (message "Verifying installed package file %s" pkg-destination)
+           (should (file-exists-p pkg-destination))
+           (should-not (zerop (nth 7 (file-attributes pkg-destination))))
+           (message "Verifying package %s was loaded" pkg)
+           (should (featurep pkg))
+           (message "Unloading and removing package %s" pkg)
+           (el-get-remove (symbol-name pkg))
+           (message "Verifying package %s was unloaded and removed" pkg)
+           (should-not (file-exists-p pkg-destination)))
+       (when (featurep pkg)
+         (unload-feature pkg))))))
 
-                                        ;(featurep 'el-get-trivial-install-test)
-                                        ;(unload-feature 'el-get-trivial-install-test)
