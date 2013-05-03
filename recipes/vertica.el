769e48d7f35fab49e6d60aed5f46f260e79593d5
Roman Scherer <roman.scherer@burningswell.com>
2013-05-03 14:50:25 +0200

    Added vertica recipe.

---
+++ recipes/vertica.el
@@ -0,0 +1,6 @@
+(:name vertica
+       :type github
+       :pkgname "r0man/vertica-el"
+       :description "Vertica SQL mode extension for Emacs"
+       :load "vertica.el"
+       :compile ("vertica.el"))
