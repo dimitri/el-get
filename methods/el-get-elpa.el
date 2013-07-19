0aaf7db4ea1af0ff3bc951b385ae7a9ba06cbfbe
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2013-07-19 15:40:38 +0200

    Minor fix: `sprintf' replaced by `format'.
    (elisp is not C ;-)
    
    Signed-off-by: Rüdiger Sonderfeld <ruediger@c-plusplus.de>

--- methods/el-get-elpa.el
+++ methods/el-get-elpa.el
@@ -119,7 +119,7 @@ the recipe, then return nil."
 (defun el-get-elpa-update-available-p (package)
   "Returns t if PACKAGE has an update available in ELPA."
   (assert (el-get-package-is-installed package) nil
-          (sprintf "Cannot update non-installed ELPA package %s" package))
+          (format "Cannot update non-installed ELPA package %s" package))
   (let* ((pkg-version
           (if (fboundp 'package-desc-version) ;; new in Emacs 24.4
               #'(lambda (pkg) (package-desc-version (car pkg)))
