e34fe972272ff6c4a65a63145b1d77ada4aee631
Noam Postavsky <npostavs@users.sourceforge.net>
2014-03-18 21:18:48 -0400

    avoid many package-refresh-contents via let-bind
    Previous flet hack is not needed since package-refresh-contents calls
    are performed by code that is under our control, namely
    el-get-elpa-update.

--- el-get.el
+++ el-get.el
@@ -829,21 +829,8 @@ itself.")
   (when (or no-prompt
             (yes-or-no-p
              "Do you really want to update all installed packages? "))
-    ;; The let and flet forms here ensure that
-    ;; `package-refresh-contents' is only called once, regardless of
-    ;; how many ELPA-type packages need to be installed. Without this,
-    ;; a refresh would happen for every ELPA package, which is totally
-    ;; unnecessary when updating them all at once.
-    (let ((refreshed nil)
-          (orig-package-refresh-contents
-           (ignore-errors (symbol-function 'package-refresh-contents))))
-      (flet ((package-refresh-contents
-              (&rest args)
-              (unless refreshed
-                (apply orig-package-refresh-contents args)
-                (setq refreshed t))))
-        ;; This is the only line that really matters
-        (mapc 'el-get-update (el-get-list-package-names-with-status "installed"))))))
+    (let ((el-get-elpa-do-refresh 'once))
+      (mapc 'el-get-update (el-get-list-package-names-with-status "installed")))))
 
 ;;;###autoload
 (defun el-get-update-packages-of-type (type)
