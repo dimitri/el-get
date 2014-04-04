939bf99dc9dee02bc98f822544848eacd8799292
Noam Postavsky <npostavs@users.sourceforge.net>
2014-04-04 18:27:50 -0400

    just do compilation for travis testing
    The current test script doesn't work at all anyway, so at least this is
    better than nothing.

--- test/run-travis-ci.sh
+++ test/run-travis-ci.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 set -ex
-cd "$(dirname "$0")"
 
-./run-ert.sh
-./run-test.sh el-get-*.el 2>/dev/null
+# for now, just require no warnings or errors during compilation
+$EMACS -Q -L . -L methods -batch --eval '(setq byte-compile-error-on-warn t)' \
+    -f batch-byte-compile *.el methods/*.el
+
+# TODO: actually run some tests
+# cd "$(dirname "$0")"
+# ./run-ert.sh
+# ./run-test.sh el-get-*.el 2>/dev/null
