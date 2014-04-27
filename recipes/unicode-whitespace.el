e84d126653096812ecd89679e4a9e82c524c5701
David Holm <dholmster@gmail.com>
2014-04-27 12:17:49 +0200

    unicode-whitespace recipe

---
+++ recipes/unicode-whitespace.el
@@ -0,0 +1,5 @@
+(:name unicode-whitespace
+       :description "Teach whitespace-mode about fancy characters."
+       :type github
+       :pkgname "rolandwalker/unicode-whitespace"
+       :depends (ucs-utils list-utils persistent-soft pcache))
