02ade6ed8dbef09707561e8952f4b10a431c5e5c
Noam Postavsky <npostavs@users.sourceforge.net>
2014-05-04 00:35:05 -0400

    Remove whitespace variables from .dir-locals.el
    The overriding whitespace-style had the annoying effect of hiding tabs
    in whitespace-mode. Furthermore, when testing with emacs -Q, it was
    triggering the unsafe local variable warning.
    
    To compensate for removing whitespace handling from emacs, add a
    whitespace check into the travis test.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,19 +3,6 @@
 
 ((nil
   (sentence-end-double-space . t)
-  (require-final-newline . t)
   (indent-tabs-mode))
  (emacs-lisp-mode
-  (byte-compile-warnings . (not cl-functions))
-  (whitespace-style face trailing lines-tail)
-  (whitespace-line-column . 80)
-  (eval ignore-errors
-        "Write-contents-functions is a buffer-local alternative to before-save-hook"
-        (add-hook 'write-contents-functions
-                  (lambda ()
-                    (delete-trailing-whitespace)
-                    nil))
-        (require 'whitespace)
-        "Sometimes the mode needs to be toggled off and on."
-        (whitespace-mode 0)
-        (whitespace-mode 1))))
+  (byte-compile-warnings . (not cl-functions))))
