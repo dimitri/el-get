6707f8c1d8b24ab088d09c5e9fcef9c10e23b733
Noam Postavsky <npostavs@users.sourceforge.net>
2014-05-03 22:41:27 -0400

    let ert shell scripts use test-utils.sh

--- test/run-ert-interactive.sh
+++ test/run-ert-interactive.sh
@@ -3,6 +3,4 @@
 source "$(dirname $0)"/test-utils.sh
 ERT_TEST="$EL_GET_LIB_DIR/test/test.el"
 
-HOME="$TEST_HOME" exec \
-    "$EMACS" -Q -L "$EL_GET_LIB_DIR" -l "$ERT_TEST" \
-    --eval "(ert t)"
+emacs_with_test_home interactive "$ERT_TEST" --eval "(ert t)"
--- test/run-ert.sh
+++ test/run-ert.sh
@@ -3,6 +3,4 @@
 source "$(dirname $0)"/test-utils.sh
 ERT_TEST="$EL_GET_LIB_DIR/test/test.el"
 
-HOME="$TEST_HOME" exec \
-    "$EMACS" -Q -batch -L "$EL_GET_LIB_DIR" -l "$ERT_TEST" \
-    -f ert-run-tests-batch-and-exit
+emacs_with_test_home batch "$ERT_TEST" -f ert-run-tests-batch-and-exit
