5b758fdff13a94fdc1449f0b111c925aaa0da9e3
Noam Postavsky <npostavs@users.sourceforge.net>
2014-06-18 21:43:11 -0400

    Don't let elpa activate el-get installed packages
    * el-get.el (el-get-do-init): add non-elpa package to
      package-activated-list so elpa thinks knows they are already
      activated.

--- el-get.el
+++ el-get.el
@@ -448,7 +448,19 @@ called by `el-get' (usually at startup) for each installed package."
                    postinit "post-init" package)
           (funcall maybe-lazy-eval `(el-get-load-package-user-init-file ',package))
           (funcall el-get-maybe-lazy-runsupp
-                   after "after" package)))
+                   after "after" package))
+        (when (and (not (eq method 'elpa)) ; if this isn't an elpa package
+                   ;; and we have any elpa packages installed
+                   (some (lambda (pkg-status)
+                           (let* ((pkg-info (cdr pkg-status))
+                                  (recipe (plist-get pkg-info 'recipe)))
+                             (and (equal (plist-get pkg-info 'status) "installed")
+                                  (eq (plist-get recipe :type) 'elpa))))
+                         (el-get-read-status-file)))
+          ;; tell elpa that this package has been activated, so it
+          ;; doesn't try to activate it's own package instead.
+          (require 'package)
+          (push (el-get-as-symbol package) package-activated-list)))
     (debug err
            (el-get-installation-failed package err)))
   ;; and call the global init hooks
