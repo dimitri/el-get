bf979624f98878c88d322def9f02f775aa94ea9c
tarao <tarao.gnn@gmail.com>
2014-11-03 04:02:36 +0900

    Change the license of the file.

--- el-get-bundle.el
+++ el-get-bundle.el
@@ -4,23 +4,9 @@
 ;; URL: https://github.com/tarao/bundle-el/tree/el-get
 ;; Version: 0.1
 ;; Keywords: emacs package install compile
+;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
-
-;;; License:
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Code:
 
