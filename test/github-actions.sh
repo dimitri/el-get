cb7a1922ad5fab6019247cc0fa8bb3b8f3edbf2d
Noam Postavsky <npostavs@users.sourceforge.net>
2026-05-27 19:40:52 -0400

    Make it easier to run github-actions locally
    Skip $GITHUB_EVENT_PATH stuff if it's missing. And in that case also
    skip defining things as nops for all but one $EMACS_VERSION.
    
    Make check-recipes real definition exist by default (override to nop
    only in case of $GITHUB_EVENT_PATH is there and non-chosen
    $EMACS_VERSION).

--- test/github-actions.sh
+++ test/github-actions.sh
@@ -5,55 +5,60 @@
 #   - check-whitespace()
 #   - ert-tests()
 
-BASE=$(cat "$GITHUB_EVENT_PATH" | jq -r '.pull_request.base.sha')
-if [ "$BASE" = 'null' ]; then
-    BASE=$(cat "$GITHUB_EVENT_PATH" | jq -r '.before')
-fi
+: ${EMACS:=emacs}
 
-HEAD=$(cat "$GITHUB_EVENT_PATH" | jq -r '.pull_request.head.sha')
-if [ "$HEAD" = 'null' ]; then
-    HEAD=$(cat "$GITHUB_EVENT_PATH" | jq -r '.after')
-fi
+# check-recipes [-W<check>...] <path>
+check-recipes() {
+    "$EMACS" -Q -L . -batch -l el-get-check -f el-get-check-recipe-batch "$@"
+}
 
-COMMIT_RANGE="${BASE}..${HEAD}"
+if [ -r "$GITHUB_EVENT_PATH" ] ; then
+   BASE=$(cat "$GITHUB_EVENT_PATH" | jq -r '.pull_request.base.sha')
+   if [ "$BASE" = 'null' ]; then
+       BASE=$(cat "$GITHUB_EVENT_PATH" | jq -r '.before')
+   fi
 
-if [ "$EMACS_VERSION" = '26.3' ]; then
-    # check-recipes [-W<check>...] <path>
-    check-recipes() {
-        emacs -Q -L . -batch -l el-get-check -f el-get-check-recipe-batch "$@"
-    }
-    check-whitespace() {
-        git --no-pager -c core.whitespace=tab-in-indent diff --check "$COMMIT_RANGE"
-    }
-    # Adapted from Emacs' build-aux/git-hooks/pre-commit.
-    check-filenames() {
-        git diff --name-only --diff-filter=A "$COMMIT_RANGE" |
-            grep -E '^-|/-|[^-+./_0-9A-Z_a-z]' |
-            while IFS= read -r new_name; do
-                case $new_name in
-                    -* | */-*)
-                        echo "$new_name: File name component begins with '-'."
-                        return 1;;
-                    *)
-                        echo "$new_name: File name does not consist of -+./_ or ASCII letters or digits."
-                        return 1;;
-                esac
-            done
-    }
-else
-    # If we have only changes to recipe files, there is no need to run
-    # with more than 1 emacs version.
-    git diff --name-only "$COMMIT_RANGE" | grep -Fvq recipes/ ||
-        { echo 'Recipe only diff, skipping tests' ; exit 0 ; }
+   HEAD=$(cat "$GITHUB_EVENT_PATH" | jq -r '.pull_request.head.sha')
+   if [ "$HEAD" = 'null' ]; then
+       HEAD=$(cat "$GITHUB_EVENT_PATH" | jq -r '.after')
+   fi
 
-    # Only need to run these for 1 version, so make them nops here
-    check-recipes() { :; }
-    check-whitespace() { :; }
-    check-filenames() { :; }
+   COMMIT_RANGE="${BASE}..${HEAD}"
+
+   if [ "$EMACS_VERSION" = '26.3' ]; then
+       check-whitespace() {
+           git --no-pager -c core.whitespace=tab-in-indent diff --check "$COMMIT_RANGE"
+       }
+       # Adapted from Emacs' build-aux/git-hooks/pre-commit.
+       check-filenames() {
+           git diff --name-only --diff-filter=A "$COMMIT_RANGE" |
+               grep -E '^-|/-|[^-+./_0-9A-Z_a-z]' |
+               while IFS= read -r new_name; do
+                   case $new_name in
+                       -* | */-*)
+                           echo "$new_name: File name component begins with '-'."
+                           return 1;;
+                       *)
+                           echo "$new_name: File name does not consist of -+./_ or ASCII letters or digits."
+                           return 1;;
+                   esac
+               done
+       }
+   else
+       # If we have only changes to recipe files, there is no need to run
+       # with more than 1 emacs version.
+       git diff --name-only "$COMMIT_RANGE" | grep -Fvq recipes/ ||
+           { echo 'Recipe only diff, skipping tests' ; exit 0 ; }
+
+       # Only need to run these for 1 version, so make them nops here
+       check-recipes() { :; }
+       check-whitespace() { :; }
+       check-filenames() { :; }
+   fi
 fi
 
 ert-tests() {
-    emacs -batch -Q $EMACS_OPT -L . -l test/el-get-tests.el -f ert-run-tests-batch-and-exit
+    "$EMACS" -batch -Q $EMACS_OPT -L . -l test/el-get-tests.el -f ert-run-tests-batch-and-exit
 }
 
 # byte-compile [-Werror] <files>...
@@ -63,7 +68,7 @@ byte-compile() {
         error_on_warning=t
         shift
     fi
-    emacs -Q -L . -L methods/ -batch \
+    "$EMACS" -Q -L . -L methods/ -batch \
           --eval "(setq byte-compile-error-on-warn $error_on_warning)" \
           -f batch-byte-compile "$@"
 }
