5c1e9c10860f35a1ee6e56b8d19505913b1c2ff2
Alex Ott <alexott@gmail.com>
2026-09-13 10:26:25 +0200

    Added new packages: Splunk & KQL modes, markdown-to-org (#3027)

---
+++ recipes/splunk-mode.el
@@ -0,0 +1,5 @@
+(:name splunk-mode
+       :description "Emacs major mode for editing Splunk SPL source code"
+       :website "https://github.com/jakewilliami/splunk-mode"
+       :type elpa
+       :repo ("melpa" . "http://melpa.org/packages/"))
